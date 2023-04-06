f19b3c0ea591e1016ded8c884ae2c5192db250b8
Yurii Poudanien <mcpchip@gmail.com>
2023-04-06 22:09:58 +0300

    * simplified duplicated code

--- browserfiplib/src/main/java/com/yupo/browserfiplib/FiPSearchView.kt
+++ browserfiplib/src/main/java/com/yupo/browserfiplib/FiPSearchView.kt
@@ -13,6 +13,7 @@ import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 
@@ -56,35 +57,35 @@ class FiPSearchView @JvmOverloads constructor(
         hint = typedArray.getString(R.styleable.FiPSearchView_fip_hint)
         backgroundColor = typedArray.getColor(
             R.styleable.FiPSearchView_fip_background_color,
-            ContextCompat.getColor(context, R.color.fip_white)
+            getColor(R.color.fip_white)
         )
         nextArrowIcon = typedArray.getDrawable(R.styleable.FiPSearchView_fip_next_icon)
-            ?: ContextCompat.getDrawable(context, R.drawable.arrow_down_24)
+            ?: getDrawable(R.drawable.arrow_down_24)
         previousArrowIcon = typedArray.getDrawable(R.styleable.FiPSearchView_fip_previous_icon)
-            ?: ContextCompat.getDrawable(context, R.drawable.arrow_up_24)
+            ?: getDrawable(R.drawable.arrow_up_24)
         closeArrowIcon = typedArray.getDrawable(R.styleable.FiPSearchView_fip_close_icon)
-            ?: ContextCompat.getDrawable(context, R.drawable.ic_close)
+            ?: getDrawable(R.drawable.ic_close)
         dividerVisibility =
             typedArray.getBoolean(R.styleable.FiPSearchView_fip_divider_visible, true)
         dividerColor =
             typedArray.getColor(
                 R.styleable.FiPSearchView_fip_divider_color,
-                ContextCompat.getColor(context, R.color.fip_divider)
+                getColor(R.color.fip_divider)
             )
 
         counterEmptyColor = typedArray.getColor(
             R.styleable.FiPSearchView_fip_counter_empty_color,
-            ContextCompat.getColor(context, R.color.fip_empty_counter_color)
+            getColor(R.color.fip_empty_counter_color)
         )
         counterMatchedColor = typedArray.getColor(
             R.styleable.FiPSearchView_fip_counter_matched_color,
-            ContextCompat.getColor(context, R.color.fip_matched_counter_color)
+            getColor(R.color.fip_matched_counter_color)
         )
         typedArray.recycle()
     }
 
     private fun initView() {
-        setBackgroundColor(ContextCompat.getColor(context, R.color.fip_background_color))
+        setBackgroundColor(getColor(R.color.fip_background_color))
         buttonNext.setOnClickListener {
             onNavigationClicked(ClickEvent.NEXT)
             webView?.findNext(true) ?: errorMessage()
@@ -190,8 +191,11 @@ class FiPSearchView @JvmOverloads constructor(
 
     private fun SearchView.changeColors(@ColorRes color: Int) {
         val editText = this.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
-        editText.setTextColor(ContextCompat.getColor(context, color))
-        editText.setHintTextColor(ContextCompat.getColor(context, color))
+        editText.setTextColor(getColor(color))
+        editText.setHintTextColor(getColor(color))
     }
+
+    private fun getDrawable(@DrawableRes resId: Int) = ContextCompat.getDrawable(context, resId)
+    private fun getColor(@ColorRes resId: Int) = ContextCompat.getColor(context, resId)
 
 }
